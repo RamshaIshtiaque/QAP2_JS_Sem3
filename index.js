4e2f9865df386aaeaccf1406c67ed1519f6ef310
RamshaIshtiaque <ramshaishtiaque@gmail.com>
2024-02-12 22:03:25 -0330

    completed step 3 and modularised the code.

--- index.js
+++ index.js
@@ -1,46 +1,68 @@
 const http = require('http');
-const fs = require('fs');
-const { EventEmitter } = require('stream');
+const path = require('path');
+
 const EventEmitter = require('events');
-const myEmitter = new EventEmitter();
+class MyEmitter extends EventEmitter{};
+const myEmitter = new MyEmitter();
 
 global.DEBUG = true;
-
 const port = 3000;
+
+myEmitter.on('route', (url) => {
+  const d = new Date();
+  if(DEBUG) console.log(`Route Event on: ${url} at ${d}`);
+  if(!fs.existsSync(path.join(__dirname, 'logs'))) {
+    fs.mkdirSync(path.join(__dirname, 'logs'));
+  }
+  fs.appendFile(path.join(__dirname, 'logs', 'route.log'), `Route Event on: ${url} at ${d}\n`, (err) => {
+    if(err) throw err;
+  });
+});
+
+//Every time a file was successfully read and write a message to the console.
+myEmitter.on('fileReadSuccess', (fileName) => {
+  console.log(`File Successfully Read: ${fileName}`);
+});
+
+//Every time a file is not available and write a message to the console.
+myEmitter.on('fileNotAvailable', (fileName) => {
+  console.error(`File Not Available: ${fileName}`);
+});
+
 
 const server = http.createServer((request, response) => {
   if (DEBUG) console.log('Request URL:' , request.url);
   let path = './views/';
   switch (request.url) {
     case '/about':
-      if (DEBUG) console.log('Handling route: /about');
+      myEmitter.emit('route' , path);
       path += 'about.html'
       if (DEBUG) console.log('Path:' , path);
-      fetchFile(path,response);
+      route.aboutPage(path, response);
       break;
     case '/contact':
-      if (DEBUG) console.log('Handling route: /contact');
+      myEmitter.emit('route' , path);
       path += 'contact.html'
       if (DEBUG) console.log('Path:' , path);
-      fetchFile(path,response);
+      route.contactPage(path, response);
       break;
     case '/products':
-      if (DEBUG) console.log('Handling route: /products');
+      myEmitter.emit('route' , path);
       path += 'products.html'
       if (DEBUG) console.log('Path:' , path);
-      fetchFile(path,response);
+      route.productsPage(path, response);
       break;
     case '/subscribe':
-      if (DEBUG) console.log('Handling route: /subscribe');
+      myEmitter.emit('route' , path);
       path += 'subscribe.html'
       if (DEBUG) console.log('Path:' , path);
-      fetchFile(path,response);
+      route.subscribePage(path, response);
       break;
     case '/':
-      if (DEBUG) console.log('Handling route: /');
+      myEmitter.emit('route' , path);
       path += 'index.html'
       if (DEBUG) console.log('Path:' , path);
-      fetchFile(path,response);
+      route.indexPage(path, response);
       break;
     default:
       if (DEBUG) console.log(`404 Not Found: ${request.url}`);
@@ -49,20 +71,15 @@ const server = http.createServer((request, response) => {
   }
 });
 
-function fetchFile(fileName, response) {
-  fs.readFile(fileName, (error, content) => {
-    if(error) {
-      response.writeHead(500, { 'Content-Type': 'text/plain' });
-      response.end('500 Internal Server Error');
-    } else {
-      response.writeHead(200, { 'Content-Type': 'text/html' });
-      response.end(content, 'utf-8');
-    }
-  });
-};
+
+server.on('error', (err) => {
+  console.error('Server Error:', err.message);
+});
 
 server.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
+
+
 
 
